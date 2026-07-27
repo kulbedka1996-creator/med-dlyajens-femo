3860d68ef09c6b65d6ac75475d5e1b1dc9b03bf4
kulbedka1996-creator <kulbedka1996@gmail.com>
2026-07-28 02:24:50 +0500

    feat: bind hanging prepositions with non-breaking space across whole site (JS TreeWalker)

--- perimenopause-reference-match/script.js
+++ perimenopause-reference-match/script.js
@@ -57,4 +57,29 @@
       }
     });
   }
+
+  /* ---- Bind hanging prepositions (неразрывный пробел после предлога) ----
+     Заменяет обычный пробел после короткого предлога/союза на \u00A0,
+     чтобы предлог не оставался висячим в конце строки.            */
+  function bindHangingPrepositions(root) {
+    var preps = ['в','с','к','у','о','и','а','но','за','на','по','об','от','до',
+                 'из','ко','со','при','для','про','без','над','под','перед',
+                 'около','между','против','через','вдоль','вокруг','ради','именно'];
+    var re = new RegExp('(^|\\s)(' + preps.join('|') + ')(\\s+)', 'gi');
+    var skip = { SCRIPT:1, STYLE:1, TEXTAREA:1, NOSCRIPT:1, CODE:1, PRE:1, KBD:1 };
+    var walker = document.createTreeWalker(root, NodeFilter.SHOW_TEXT, {
+      acceptNode: function (node) {
+        var p = node.parentNode;
+        if (!p || skip[p.nodeName]) return NodeFilter.FILTER_REJECT;
+        return re.test(node.nodeValue) ? NodeFilter.FILTER_ACCEPT
+                                       : NodeFilter.FILTER_REJECT;
+      }
+    });
+    var nodes = [];
+    while (walker.nextNode()) nodes.push(walker.currentNode);
+    nodes.forEach(function (node) {
+      node.nodeValue = node.nodeValue.replace(re, '$1$2\u00A0');
+    });
+  }
+  bindHangingPrepositions(document.body);
 })();
